e701a6c09e1704b1060bff59ca3412ea92b8320a
Patric Eckhart <mail@patriceckhart.com>
2018-06-28 18:26:17 +0200

    lightbox for images, parallax effect and animate.css for all content elements added

--- Resources/Private/JavaScript/scripts.js
+++ Resources/Private/JavaScript/scripts.js
@@ -337,7 +337,39 @@ function onScrollInit( items, trigger ) {
 onScrollInit( $('.os-animation') );
 onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
 
+function isInViewport(node) {
+    var rect = node.getBoundingClientRect();
+    return (
+        (rect.height > 0 || rect.width > 0) &&
+        rect.bottom >= 0 &&
+        rect.right >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.left <= (window.innerWidth || document.documentElement.clientWidth);
+    )
+}
+
 $(document).ready(function(){
     $.cookieBar({
     });
+    $('.parallax').each(function(index) {
+        var imageSrc = $(this).data('image-src');
+        var imageHeight = $(this).data('height');
+        $(this).css('background-image','url(' + imageSrc + ')');
+        $(this).css('height', imageHeight);
+    });
+    $(window).scroll(function() {
+        var scrolled = $(window).scrollTop();
+        $('.parallax').each(function(index, element) {
+            var initY = $(this).offset().top;
+            var height = $(this).height();
+            var endY  = initY + $(this).height();
+
+            var visible = isInViewport(this);
+            if(visible) {
+                var diff = scrolled - initY;
+                var ratio = Math.round((diff / height) * 100);
+                $(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px');
+            }
+        })
+    })
 });
