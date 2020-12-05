b56021588912ea37cd290f04fdbb710ecb9d34e3
Patric Eckhart <mail@patriceckhart.com>
2020-12-05 20:00:04 +0100

    no message

--- Classes/Fusion/VideoBackground.php
+++ Classes/Fusion/VideoBackground.php
@@ -7,11 +7,13 @@ use Neos\Fusion\FusionObjects\AbstractFusionObject;
 class VideoBackground extends AbstractFusionObject {
 
     /**
-     * @return string
+     * @return array
      */
     public function evaluate() {
 
         $link = $this->fusionValue('videourl');
+        $platform = false;
+        $result = [];
 
         if($link) {
 
@@ -19,16 +21,19 @@ class VideoBackground extends AbstractFusionObject {
                 $linkString = explode('/', $link);
                 $youtubeId = array_pop($linkString);
                 $link = 'https://www.youtube.com/embed/'.$youtubeId.'?controls=0&showinfo=0&rel=0&autoplay=1&mute=1&loop=1&playlist='.$youtubeId;
+                $platform = 'youtube';
             }
 
             if (strpos($link, 'watch') !== false) {
                 $youtubeId = substr($link, strpos($link, "=") + 1);
                 $link = 'https://www.youtube.com/embed/'.$youtubeId.'?controls=0&showinfo=0&rel=0&autoplay=1&mute=1&loop=1&playlist='.$youtubeId;
+                $platform = 'youtube';
             }
 
             if (strpos($link, 'youtu.be') !== false) {
                 $youtube_id = str_replace('https://youtu.be/', '', $link);
                 $link = 'https://www.youtube.com/embed/'.$youtube_id.'?controls=0&showinfo=0&rel=0&autoplay=1&mute=1&loop=1&playlist='.$youtubeId;
+                $platform = 'youtube';
             }
 
             if (strpos($link, 'player') !== false) {
@@ -36,17 +41,21 @@ class VideoBackground extends AbstractFusionObject {
                     $vimeoId = $matches[1];
                 }
                 $link = 'https://player.vimeo.com/video/'.$vimeoId.'?autoplay=1&loop=1&title=0&portrait=0&byline=0';
+                $platform = 'vimeo';
             }
 
             if (strpos($link, 'vimeo') !== false) {
                 $linkString = explode('/', $link);
                 $vimeoId = array_pop($linkString);
                 $link = 'https://player.vimeo.com/video/'.$vimeoId.'?autoplay=1&loop=1&title=0&portrait=0&byline=0';
+                $platform = 'vimeo';
             }
 
         }
 
-        return $link;
+        $result = ['videourl' => $link, 'platform' => $platform];
+
+        return $result;
 
     }
 }
