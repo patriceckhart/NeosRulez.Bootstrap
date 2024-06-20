e7d61813514758bc44bf7e578dacf9541658b044
Patric Eckhart <mail@patriceckhart.com>
2024-06-20 19:15:42 +0200

    parallax sourceset optimized

--- Resources/Private/JavaScript/parallax.js
+++ Resources/Private/JavaScript/parallax.js
@@ -120,12 +120,12 @@
 		if (slider.length == 0)
 			this.$slider = $(`
 					<picture>
-						<source type="image/webp" media="(min-width: 1200px)" sizes="(min-width: 300px) 300px, 100vw" />
-						<source type="image/webp" media="(min-width: 576px)" sizes="(min-width: 300px) 300px, 100vw" />
-						<source type="image/webp" media="(max-width: 575px)" sizes="(min-width: 300px) 300px, 100vw" />
-						<source type="image/jpeg" media="(min-width: 1200px)" sizes="(min-width: 300px) 300px, 100vw" />
-						<source type="image/jpeg" media="(min-width: 576px)" sizes="(min-width: 300px) 300px, 100vw" />
-						<source type="image/jpeg" media="(max-width: 575px)" sizes="(min-width: 300px) 300px, 100vw" />
+						<source type="image/webp" media="(min-width: 1200px)" sizes="(min-width: 1200px) 1200px, 100vw" />
+						<source type="image/webp" media="(min-width: 576px)" sizes="(min-width: 576px) 576px, 100vw" />
+						<source type="image/webp" media="(max-width: 575px)" sizes="(max-width: 575px) 575px, 100vw" />
+						<source type="image/jpeg" media="(min-width: 1200px)" sizes="(min-width: 1200px) 1200px, 100vw" />
+						<source type="image/jpeg" media="(min-width: 576px)" sizes="(min-width: 576px) 576px, 100vw" />
+						<source type="image/jpeg" media="(max-width: 575px)" sizes="(max-width: 575px) 575px, 100vw" />
 						<img />
 					</picture>
 				`).prependTo(this.$mirror);
@@ -168,7 +168,7 @@
 			this.$slider[0].querySelectorAll('source')[4].srcset = this.parallaxValue.srcsetTablet;
 			this.$slider[0].querySelectorAll('source')[5].srcset = this.parallaxValue.srcsetMobile;
 			this.$slider[0].querySelector('img').src = this.imageSrc;
-		this.$slider[0].querySelector('img').alt = this.parallaxId;
+			this.$slider[0].querySelector('img').alt = this.parallaxId;
 
 		if (this.naturalHeight && this.naturalWidth || this.$slider[0].querySelector('img').complete || slider.length > 0) {
 			this.$slider.trigger('load');
