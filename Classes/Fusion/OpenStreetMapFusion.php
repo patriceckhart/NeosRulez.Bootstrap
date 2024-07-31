8f14fb1b2222d48bb588c602329bcc97235c4a94
Patric Eckhart <mail@patriceckhart.com>
2024-07-31 08:52:09 +0200

    established an openstreetmap result cache

--- Classes/Fusion/OpenStreetMapFusion.php
+++ Classes/Fusion/OpenStreetMapFusion.php
@@ -1,8 +1,10 @@
 <?php
 namespace NeosRulez\Bootstrap\Fusion;
 
+use Neos\Cache\Frontend\StringFrontend;
 use Neos\Flow\Annotations as Flow;
 use Doctrine\ORM\Mapping as ORM;
+use Neos\Flow\Exception;
 use Neos\Fusion\FusionObjects\AbstractFusionObject;
 
 use GuzzleHttp\Client;
@@ -10,6 +12,12 @@ use GuzzleHttp\Psr7\Request;
 
 class OpenStreetMapFusion extends AbstractFusionObject
 {
+
+    /**
+     * @var StringFrontend
+     * @Flow\Inject
+     */
+    protected $openStreetMapCache;
 
     /**
      * @return string
@@ -24,7 +32,9 @@ class OpenStreetMapFusion extends AbstractFusionObject
 
         if($address && $zip && $city && $country) {
             $wayFromOSMData = $this->getWayFromOSMData($address, $zip, $city, $country);
-            $string = 'https://www.openstreetmap.org/export/embed.html?bbox=' . $wayFromOSMData['boundingbox'][2] . '%2C' . $wayFromOSMData['boundingbox'][0] . '%2C' . $wayFromOSMData['boundingbox'][3] . '%2C' . $wayFromOSMData['boundingbox'][1] . '&amp;layer=mapnik&amp;marker=' . $wayFromOSMData['lat'] . '%2C' . $wayFromOSMData['lon'];
+            if(!empty($wayFromOSMData)) {
+                $string = 'https://www.openstreetmap.org/export/embed.html?bbox=' . $wayFromOSMData['boundingbox'][2] . '%2C' . $wayFromOSMData['boundingbox'][0] . '%2C' . $wayFromOSMData['boundingbox'][3] . '%2C' . $wayFromOSMData['boundingbox'][1] . '&amp;layer=mapnik&amp;marker=' . $wayFromOSMData['lat'] . '%2C' . $wayFromOSMData['lon'];
+            }
         }
 
         return $string;
@@ -61,10 +71,22 @@ class OpenStreetMapFusion extends AbstractFusionObject
      */
     private function fetchOSMData(string $address, string $zip, string $city, string $country): array
     {
-        $client = new Client();
-        $request = new Request('GET', 'https://nominatim.openstreetmap.org/search?street=' . $this->paramReplacement($address) . '&city=' . $this->paramReplacement($city) . '&country=' . $this->paramReplacement($country) . '&postalcode=' . $this->paramReplacement($zip) . '&format=json');
-        $res = $client->sendAsync($request)->wait();
-        return json_decode($res->getBody(), true);
+        $cacheIdentifier = sha1($address . $zip . $city . $country);
+
+        if ($this->openStreetMapCache->has($cacheIdentifier)) {
+            return json_decode($this->openStreetMapCache->get($cacheIdentifier), true);
+        }
+
+        try {
+            $client = new Client();
+            $request = new Request('GET', 'https://nominatim.openstreetmap.org/search?street=' . $this->paramReplacement($address) . '&city=' . $this->paramReplacement($city) . '&country=' . $this->paramReplacement($country) . '&postalcode=' . $this->paramReplacement($zip) . '&format=json');
+            $res = $client->sendAsync($request)->wait();
+            $result = $res->getBody();
+            $this->openStreetMapCache->set($cacheIdentifier, json_encode(json_decode($result, true)));
+            return json_decode($result, true);
+        } catch(Exception $exception) {
+            return [];
+        }
     }
 
     /**
